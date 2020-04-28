8d72299e2f79a7bcf0f211cae85bfc82df583532
afif.alfiano <afif.alfiano@uii.ac.id>
2020-04-28 21:51:28 +0700

    activiy from component

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, AfterContentInit, ContentChildren, QueryList, Input } from '@angular/core';
+import { CarouselItemComponent } from '../carousel-item/carousel-item.component';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent implements AfterContentInit {
+
+  @ContentChildren(CarouselItemComponent) carouselItemsList: QueryList<CarouselItemComponent>;
+
+  @Input() delay = 500;
+
+  ngAfterContentInit() {
+    const carouselItems = this.carouselItemsList.toArray();
+    let count = 0;
+    const max = carouselItems.length;
+    setInterval(() => {
+      const i = count % max;
+      carouselItems.forEach((item) => item.isActive = false);
+      carouselItems[i].isActive = true;
+      count += 1;
+    }, this.delay);
+  }
+  }
+
