523cc4ee6f9d3dd5f20e87fafc1f3fdf128a1316
afif.alfiano <afif.alfiano@uii.ac.id>
2020-04-30 21:23:09 +0700

    Learn Rxjs on Form Reactive Angular

---
+++ src/app/form-app/form-app.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+
+import { map, filter } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-form-app',
+  templateUrl: './form-app.component.html',
+  styleUrls: ['./form-app.component.css']
+})
+export class FormAppComponent implements OnInit {
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.form = fb.group({
+      comment: this.comment,
+      name: this.name,
+      email: this.email
+    });
+
+    // this.form.valueChanges
+    //   .subscribe( data => {
+    //     if (this.form.valid) {
+    //       data.comment = data.comment.replace(/<(?:.|\n)*?>/gm, '');
+    //       data.lastUpdateTS = new Date();
+    //       console.log(JSON.stringify(data));
+    //     }
+    //   });
+
+    this.form.valueChanges
+      .pipe(
+        filter(data => this.form.valid),
+        map(data => {
+          data.comment = data.comment.replace(/<(?:.|\n)*?>/gm, '');
+          return data;
+        }),
+        map(data => {
+          data.lastUpdateTS = new Date();
+          return data;
+        })
+      )
+      .subscribe(data => console.log(JSON.stringify(data)));
+  }
+  form: FormGroup;
+  comment = new FormControl('', Validators.required);
+  name = new FormControl('', Validators.required);
+  email = new FormControl('', [
+    Validators.required,
+    Validators.pattern('[^ @]*@[^ @]*')
+  ]);
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log('Form Submitted');
+  }
+
+}
