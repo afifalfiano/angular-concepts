5522de619257539860b9484ea3f935d3e18b6e92
afif.alfiano <afif.alfiano@uii.ac.id>
2020-05-02 22:21:04 +0700

    Model drive validation
    We can add validators to our model form which check each field for validity.
    
    Can render the controls with styling to show the user when fields are invalid.
    
    Finally, we can add validation error messages so the user knows how to make the form valid again.
    
    Next up we’ll look at how to submit and reset a model-driven form.

--- src/app/modal-form/modal-form.component.ts
+++ src/app/modal-form/modal-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-form',
@@ -13,20 +13,48 @@ export class ModalFormComponent implements OnInit {
     'German',
   ];
   myform: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  language: FormControl;
   constructor() {
 
    }
 
   ngOnInit() {
-    this.myform = new FormGroup({
-      name: new FormGroup({
-        firstName: new FormControl(),
-        lastName: new FormControl()
-      }),
-      email: new FormControl(),
-      password: new FormControl(),
-      language: new FormControl()
-    });
-  }
+    this.createFormControls();
+    this.createForm();
 
+}
+
+createFormControls(){
+  this.myform = new FormGroup({
+    name : new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required)
+    }),
+    email: new FormControl('',[
+      Validators.required,
+      Validators.pattern('[^ @]*@[^ @]*')
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.minLength(8)
+    ]),
+    language: new FormControl()
+  });
+}
+
+createForm(){
+  this.myform = new FormGroup({
+    name: new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName,
+    }),
+    email: this.email,
+    password: this.password,
+    language: this.language
+  });
+}
 }
